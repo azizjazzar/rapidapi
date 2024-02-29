bf5bc898581069b51426291cc14434dd10e17532
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-02-29 18:58:32 +0100

    updates

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
-const connectDB = require("./config/db");
-const hostname = 'localhost';
-const port = 9090;
 const app = express(); 
-const stripe = require("stripe")('sk_test_51OErmACis87pjNWpHjxy4jOfBeV5X2cD3bB2op5qNVdo8OY7pqpqJh235cFlSwbjNxfjsz6FMZAD1EVCWJs2kyDq00LYDaUrax');
-connectDB();
 app.use(express.json());
-app.set('views','./views')
-app.set('view engine','ejs')
+
 
 app.get("/", (req, res) => {
-  res.render("home")
+  res.send("aaaaa")
 });
 
-app.use("/api/auth", require("./routes/auth"));
+app.use("/api/", require("./routes/auth"));
 
 
 
